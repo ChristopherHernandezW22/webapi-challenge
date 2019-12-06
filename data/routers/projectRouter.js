c25f200c2bc241478112bbe3a5032967e436ad0a
Christopher Hernandez <cerberean661@gmail.com>
2019-12-06 11:16:33 -0800

    post, delete functional

--- data/routers/projectRouter.js
+++ data/routers/projectRouter.js
@@ -18,5 +18,80 @@ router.get('/', (req, res) => {
         })
 });
 
+router.get('/:id', validateProjectId, (req, res) => {
+    const { id } = req.params;
+    Project.get(id)
+        .then(project => {
+            res
+                .status(200)
+                .json(project)
+        })
+        .catch(error => {
+            console.log(error);
+            res
+                .status(500)
+                .json({error:"Failed to reach the server."})
+        })
+});
+
+router.post('/', (req, res) => {
+    const text = req.body;
+    Project.insert(text)
+        .then(text => {
+            console.log(text);
+            res
+                .status(201)
+                .json(text)
+        })
+        .catch(error => {
+            console.log(error);
+            res
+                .status(500)
+                .json({error:"Failed to reach the server."})
+        })
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    Project.remove(id)
+        .then(removed => {
+            if(removed) {
+                res
+                    .status(204)
+                    .json({message: "ID deleted."})
+            } else {
+                res
+                    .status(404)
+                    .json({error:"That entry ID does not exist."})
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res
+                .status(500)
+                .json({error:"Failed to reach the server."})
+        })
+});
+
+function validateProjectId(req, res, next) {
+    const { id } = req.params;
+    Project.get(id)
+        .then(project => {
+            if(project){
+                req.project = project;
+                next();
+            } else {
+                res
+                    .status(404)
+                    .json({error:"User ID does not exist."})
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res
+                .status(500)
+                .json({error:"Failed to reach the server."})
+        })
+}
 
 module.exports = router;
